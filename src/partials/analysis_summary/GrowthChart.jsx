9106ad9eb1e86ba526d51f6b144739f86687e7d1
pppompompurin <vldkshlot@naver.com>
2024-08-02 19:41:42 +0900

    dh

--- src/partials/analysis_summary/GrowthChart.jsx
+++ src/partials/analysis_summary/GrowthChart.jsx
@@ -17,7 +17,7 @@ function GrowthChart() {
   return (
     <div className="col-span-full xl:col-span-8 bg-white dark:bg-gray-800 shadow-lg rounded-sm border border-gray-200 dark:border-gray-700">
       <header className="px-5 py-4 border-b border-gray-100 dark:border-gray-700">
-        <h2 className="font-semibold text-gray-800 dark:text-gray-100">일주일 성장 그래프 (입면적) </h2>
+        <h2 className="font-semibold text-gray-800 dark:text-gray-100">성장 그래프 by 입면적 </h2>
       </header>
       <div className="p-3">
         <div className="flex items-start mb-4">
